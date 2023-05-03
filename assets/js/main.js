bfbc7ed1820b1d4f804d6073d5ab71cbf6493736
Mabel Saavedra <mabel.saavedra03@inacapmail.cl>
2023-05-02 22:13:34 -0400

    Sesion 7 Métodos del prototipo String

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+// console.log("Funciones Cadenas de Caracteres")
+// var nombreApellido = prompt("Ingrese su nombre y apellido")
+
+// //ReplaceAll
+// // console.log(nombreApellido.replaceAll(" ",""))
+// // console.log(nombreApellido.replaceAll("O","1"))
+// // console.log(nombreApellido.replaceAll("Carlos","Luis"))
+// // console.log(nombreApellido.replaceAll("é","e").replaceAll("í","i"))
+
+// // //Replace
+// // console.log(nombreApellido.replace("Carlos","Luis"));
+
+
+// //Trim
+// console.log(nombreApellido.trim());
+
+// //Trimstart
+// console.log(nombreApellido.trimStart());
+
+// //TrimEnd
+// console.log(nombreApellido.trimEnd())
+
+
+
+
+var numero= prompt("Ingrese un número");
+
+var numero2 = 1_500_000
+console.log(numero2);
+
+
+if(numero > 1_000_000) {
+    alert("El número es mayor a 1 millón")
+} else {
+    alert("El número es igual o menos a 1 millon")
+}
+
+
+
+
+
